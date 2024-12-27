5dac299000599a0b623efd5b193d1dc36bbcfbe7
rutyak <rutikkhandekar123@gmail.com>
2024-12-27 15:48:10 +0530

    new

--- Backend/index.js
+++ Backend/index.js
@@ -13,15 +13,14 @@ const app = express();
 const port = process.env.PORT;
 const url = process.env.MongoDB_URL;
 
-
 const corsOptions = {
-  origin: 'http://localhost:3000', 
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  credentials: true,  
-  allowedHeaders: ['Content-Type', 'Authorization'],  
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -34,7 +33,7 @@ mongoose
     console.error("error in connection...", error);
   });
 
-app.options('*', cors());
+app.options("*", cors());
 
 app.use(candidateRouter);
 app.use(employeeRouter);
